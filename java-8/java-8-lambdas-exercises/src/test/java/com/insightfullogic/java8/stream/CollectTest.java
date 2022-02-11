745588cfa4037891eb2173821238c41afab56698
Castlein <Castlebin@gmail.com>
2022-02-11 14:57:46 +0800

    collect toMap key冲突会抛出异常

---
+++ java-8/java-8-lambdas-exercises/src/test/java/com/insightfullogic/java8/stream/CollectTest.java
@@ -0,0 +1,26 @@
+package com.insightfullogic.java8.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class CollectTest {
+
+    /**
+     * collect toMap key冲突会抛出异常
+     *
+     * Function.identity() 表示保持原样， num -> num 不做转换
+     */
+    @Test(expected = IllegalStateException.class)
+    public void testCollectToMap() {
+        List<String> nums = Arrays.asList("1", "2", "1", "3");
+        Map<String, String> map = nums.stream()
+                .collect(Collectors.toMap(num -> num, Function.identity()));
+    }
+
+}
