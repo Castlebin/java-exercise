81cd7e65b575e20221546c9af220c4baa85eafa6
Castlein <Castlebin@gmail.com>
2023-03-11 20:19:05 +0800

    CyclicBarrier 是一个同步屏障，它可以在多个线程之间建立一个屏障， 当所有线程都到达屏障时，屏障才会开门，所有线程才会继续执行。
    CyclicBarrier 的字面意思是可循环（Cyclic）使用的屏障（Barrier）。
    通过 reset() 方法可以循环使用这个屏障。
    
    CyclicBarrier 比 CountDownLatch 更加灵活，因为它可以循环使用，并且提供额外的能力。

--- ArtConcurrentBook/source-code/src/main/java/chapter08/CyclicBarrierTest3.java
+++ ArtConcurrentBook/source-code/src/main/java/chapter08/CyclicBarrierTest3.java
@@ -4,7 +4,6 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 /**
- * 
  * @author tengfei.fangtf
  * @version $Id: CyclicBarrierTest3.java, v 0.1 2015-8-1 上午12:09:37 tengfei.fangtf Exp $
  */
@@ -14,7 +13,6 @@ public class CyclicBarrierTest3 {
 
     public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
         Thread thread = new Thread(new Runnable() {
-
             @Override
             public void run() {
                 try {
@@ -23,11 +21,15 @@ public class CyclicBarrierTest3 {
                 }
             }
         });
+
         thread.start();
+
         thread.interrupt();
+
         try {
             c.await();
         } catch (Exception e) {
+            // 可以判断任务是否被中断过
             System.out.println(c.isBroken());
         }
     }
--- ArtConcurrentBook/source-code/src/main/java/chapter08/CyclicBarrierTest_BankWaterService.java
+++ ArtConcurrentBook/source-code/src/main/java/chapter08/CyclicBarrierTest_BankWaterService.java
@@ -1,4 +1,4 @@
-package com.heller.jcip.ch08;
+package chapter08;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CyclicBarrier;
