65521eaefbec6bc43dcbd1eeb16078a838fb1c6e
Castlein <Castlebin@gmail.com>
2023-03-11 20:18:33 +0800

    Exchanger 是一个用于线程间协作的工具类。它提供一个同步点，在这个同步点，两个线程可以交换彼此的数据。     这两个线程通过 exchange 方法交换数据，如果第一个线程先执行 exchange() 方法，它会一直等待第二个线程也执行 exchange()，     当两个线程都到达同步点时，这两个线程就可以交换数据，将本线程生产出来的数据传递给对方。

--- ArtConcurrentBook/source-code/src/main/java/chapter08/ExchangerTest.java
+++ ArtConcurrentBook/source-code/src/main/java/chapter08/ExchangerTest.java
@@ -1,46 +1,42 @@
 package chapter08;
 
 import java.util.concurrent.Exchanger;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
- * 
- * @author tengfei.fangtf
- * @version $Id: ExchangerTest.java, v 0.1 2015-8-1 上午12:10:37 tengfei.fangtf Exp $
+ Exchanger 是一个用于线程间协作的工具类。它提供一个同步点，在这个同步点，两个线程可以交换彼此的数据。
+    这两个线程通过 exchange 方法交换数据，如果第一个线程先执行 exchange() 方法，它会一直等待第二个线程也执行 exchange()，
+    当两个线程都到达同步点时，这两个线程就可以交换数据，将本线程生产出来的数据传递给对方。
  */
 public class ExchangerTest {
 
-    private static final Exchanger<String> exgr       = new Exchanger<String>();
-
-    private static ExecutorService         threadPool = Executors.newFixedThreadPool(2);
-
     public static void main(String[] args) {
+        /*
+         Exchanger 是两个线程之间进行数据交换的工具，所以如果是奇数个线程，有一个线程是无法交换数据的，会阻塞。
+         Exchanger 是可以多个线程之间交换数据的，但是只能是两两之间交换数据。
+         而且，多个线程，容易混乱，你不知道到底是哪两个线程交换了数据，所以，Exchanger 一般用于两个线程之间交换数据。
+         */
 
-        threadPool.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    String A = "银行流水A";// A录入银行流水数据
-                    exgr.exchange(A);
-                } catch (InterruptedException e) {
-                }
+        Exchanger<String> exchanger = new Exchanger<>();
+
+        new Thread(() -> {
+            try {
+                String a = "银行流水A";
+                String t = exchanger.exchange(a);
+                System.out.println("A -> " + t);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
+        }).start();
 
-        threadPool.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    String B = "银行流水B";// B录入银行流水数据
-                    String A = exgr.exchange("B");
-                    System.out.println("A和B数据是否一致：" + A.equals(B) + "，A录入的是：" + A + "，B录入是：" + B);
-                } catch (InterruptedException e) {
-                }
+        new Thread(() -> {
+            try {
+                String b = "银行流水B";
+                String a = exchanger.exchange(b);
+                System.out.println("A和B数据是否一致：" + a.equals(b) + ", A录入的是：" + a + ", B录入的是：" + b);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
-
-        threadPool.shutdown();
-
+        }).start();
     }
+
 }
