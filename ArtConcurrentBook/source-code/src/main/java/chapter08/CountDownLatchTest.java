f644f7f661d39c7d98b0dc7b1d9f7b390be8f5bd
Castlein <Castlebin@gmail.com>
2023-03-11 20:18:45 +0800

    CountDownLatch 是一个同步工具类，它允许一个或多个线程一直等待，直到其他线程的操作执行完后再执行。
    CountDownLatch 的构造方法接收一个 int 类型的参数作为计数器，如果你想等待 N 个点完成，这里就传入 N。
    CountDownLatch 的 countDown 方法会将计数器减 1（调用 countDown 方法的线程不会阻塞），CountDownLatch 的 await 方法会阻塞当前线程，
    直到计数器的值为 0。

--- ArtConcurrentBook/source-code/src/main/java/chapter08/CountDownLatchTest.java
+++ ArtConcurrentBook/source-code/src/main/java/chapter08/CountDownLatchTest.java
@@ -2,28 +2,35 @@ package chapter08;
 
 import java.util.concurrent.CountDownLatch;
 
-/**
- * 
- * @author tengfei.fangtf
- * @version $Id: CountDownLatchTest.java, v 0.1 2015-8-1 上午12:08:52 tengfei.fangtf Exp $
+import com.heller.jcip.PrimeNumberGenerator;
+
+/*
+CountDownLatch 是一个同步工具类，它允许一个或多个线程一直等待，直到其他线程的操作执行完后再执行。
+
+CountDownLatch 的构造方法接收一个 int 类型的参数作为计数器，如果你想等待 N 个点完成，这里就传入 N。
+CountDownLatch 的 countDown 方法会将计数器减 1（调用 countDown 方法的线程不会阻塞），CountDownLatch 的 await 方法会阻塞当前线程，
+直到计数器的值为 0。
  */
 public class CountDownLatchTest {
 
-    static CountDownLatch c = new CountDownLatch(2);
-
     public static void main(String[] args) throws InterruptedException {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(1);
-                c.countDown();
-                System.out.println(2);
-                c.countDown();
-            }
+        CountDownLatch latch = new CountDownLatch(2);
+
+        new Thread(() -> {
+            // 模拟一下耗时任务
+            PrimeNumberGenerator.longTimeJob(500000);
+            System.out.println("parser1 finish");
+            latch.countDown();
         }).start();
 
-        c.await();
-        System.out.println("3");
-    }
+        new Thread(() -> {
+            // 模拟一下耗时任务
+            PrimeNumberGenerator.longTimeJob(1000000);
+            System.out.println("parser2 finish");
+            latch.countDown();
+        }).start();
 
+        latch.await();
+        System.out.println("all parser finish");
+    }
 }
