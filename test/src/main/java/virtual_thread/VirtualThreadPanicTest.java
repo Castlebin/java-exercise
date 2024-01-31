e47f175767f0b3c9611644f8d4c2cfe2b2fbffcd
Castlebin <Castlebin@gmail.com>
2024-01-31 13:40:24 +0800

    Java 的线程、虚拟线程 中发生异常，都只会导致当前线程退出，不会影响其他线程，更不会导致程序退出。 go 会，它需要在 goroutine 内部，采用 defer + recover 的方式去捕获并处理 panic,才不会导致程序整个退出

---
+++ test/src/main/java/virtual_thread/VirtualThreadPanicTest.java
@@ -0,0 +1,44 @@
+package virtual_thread;
+
+// Java 的线程、虚拟线程 中发生异常，都只会导致当前线程退出，不会影响其他线程，更不会导致程序退出。
+// （go 会，它需要在 goroutine 内部，采用 defer + recover 的方式去捕获并处理 panic,才不会导致程序整个退出）
+public class VirtualThreadPanicTest {
+    public static void main(String[] args) {
+        Thread t = Thread.ofVirtual().unstarted(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("p: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                if (i == 5) {
+                    throw new RuntimeException("Panic");
+                }
+            }
+        });
+        t.start();
+
+        Thread t2 = Thread.ofVirtual().unstarted(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("n: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        t2.start();
+
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("m: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
